291fe4f45f6e120904120daf07fe39e01ad15a53
PC <PC@PC-PC>
2013-09-12 10:35:35 -0300

    teste3

--- TESTE/src/LOG.java
+++ TESTE/src/LOG.java
@@ -1,4 +1,6 @@
 
 public class LOG {
-
+  public static void main(String[] args) {
+	System.out.println();
+}
 }
--- TESTE/src/Teste2.java
+++ TESTE/src/Teste2.java
@@ -1,4 +1,6 @@
 
 public class Teste2 {
-
+public static void main(String[] args) {
+	
+}
 }
